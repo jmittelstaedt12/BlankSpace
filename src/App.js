6697bbd0fb615e73fb0d3811468bc8627451a9e6
MagicalLime <rangeshabijit@gmail.com>
2019-04-27 04:12:35 -0400

    test123

--- src/App.js
+++ src/App.js
@@ -4,10 +4,6 @@ function App() {
   return (
     <div className="App">
       <header className="App-header">
-
-      <p>
-      change
-      </p>
         <p>
           Edit <code>src/App.js</code> and save to reload.
         </p>
