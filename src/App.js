65c1da858e3237eb8d5938331ff820f669e56806
Jacob Mittelstaedt <jacobmittelstaedt@gmail.com>
2019-04-27 13:55:22 -0400

    added materialize

--- src/App.js
+++ src/App.js
@@ -2,7 +2,22 @@ import React from 'react';
 import './App.css';
 function App() {
   return (
-    <div className="App">
+    <div>
+      <div className="row">
+        <div className="col s3 m3 l3">
+          <div className="card blue-grey darken-1">
+            <div className="card-content white-text">
+              <span className="card-title">Card Title</span>
+              <p>I am a very simple card. I am good at containing small bits of information.
+              I am convenient because I require little markup to use effectively.</p>
+            </div>
+            <div className="card-action">
+              <a href="#">This is a link</a>
+              <a href="#">This is a link</a>
+            </div>
+          </div>
+        </div>
+      </div>
       <header className="App-header">
         <p>
           Edit <code>src/App.js</code> and save to reload.
