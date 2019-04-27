156ccc7da32ec319d6f1919bdcb07ccab32cb9e6
Jacob Mittelstaedt <jacobmittelstaedt@gmail.com>
2019-04-27 15:41:27 -0400

    added navbar and business info page

--- src/App.js
+++ src/App.js
@@ -1,13 +1,51 @@
-import React from 'react';
+import React, { Component } from 'react';
 import './App.css';
 import NavBar from './components/NavBar.js'
-function App() {
-  return (
-    <div className="App">
-      <NavBar/>
-      <div className="card" style={{'backgroundColor': '#19a96'}}>hey</div>
-    </div>
-  );
+class App extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      value: ''
+    };
+
+    this.handleChange = this.handleChange.bind(this);
+    this.handleSubmit = this.handleSubmit.bind(this);
+  }
+
+  handleChange(event) {
+    this.setState({value: event.target.value});
+  }
+
+  handleSubmit(event) {
+    alert('An essay was submitted: ' + this.state.value);
+    event.preventDefault();
+  }
+
+  render() {
+    return (
+      <div className="App">
+        <div className="container">
+          <NavBar/>
+          <h2 style={{'font-weight': '500'}}>Tell us about your business.</h2>
+          <form>
+            <label className="inputLabel">
+              Name of business
+              <div className="row">
+                <input type="text" name="name" className="col s6 offset-s3"/>
+              </div>
+            </label>
+            <label className="inputLabel">
+              What do you hope to find out  through your BlankSpace Experience?
+              <div className="row">
+                <textarea value={this.state.value} onChange={this.handleChange} className="col s6 offset-s3" style={{height: '200px'}}/>
+              </div>
+            </label>
+            <input type="submit" value="Location Options" />
+          </form>
+        </div>
+      </div>
+    );
+  }
 }
 
 export default App;
--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -3,14 +3,14 @@ import React from 'react';
 function NavBar() {
   return (
     <nav>
-    <div className="nav-wrapper" style={{'backgroundColor': '#ffffff'}}>
-      <a href="#" className="brand-logo">Logo</a>
-      <ul id="nav-mobile" className="right hide-on-med-and-down">
-        <li><a href="sass.html">Sass</a></li>
-        <li><a href="badges.html">Components</a></li>
-        <li><a href="collapsible.html">JavaScript</a></li>
-      </ul>
-    </div>
+      <div className="nav-wrapper" style={{'backgroundColor': '#ffffff'}}>
+        <img className="brand-logo center" src={require('../assets/Blank\ Space\ Horizontal.png')}/>
+        <ul id="nav-mobile" className="right hide-on-med-and-down">
+          <li><a href="sass.html">Sass</a></li>
+          <li><a href="badges.html">Components</a></li>
+          <li><a href="collapsible.html">JavaScript</a></li>
+        </ul>
+      </div>
   </nav>
   );
 }
