1217978163a52ff1830c3ef539ccc0d3051068e5
Jacob Mittelstaedt <jacobmittelstaedt@gmail.com>
2019-04-28 10:15:10 -0400

    fixed labels

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -5,11 +5,6 @@ function NavBar() {
     <nav>
       <div className="nav-wrapper" style={{'backgroundColor': '#ffffff'}}>
         <img className="brand-logo center" src={require('../assets/Blank\ Space\ Horizontal.png')}/>
-        <ul id="nav-mobile" className="right hide-on-med-and-down">
-          <li><a href="sass.html">Sass</a></li>
-          <li><a href="badges.html">Components</a></li>
-          <li><a href="collapsible.html">JavaScript</a></li>
-        </ul>
       </div>
   </nav>
   );
